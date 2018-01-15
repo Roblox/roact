f0a9981799633a002b1047391b1405111a8929d0
AmaranthineCodices <31936135+AmaranthineCodices@users.noreply.github.com>
2018-01-15 16:23:03 -0500

    Allow (and ignore) booleans as children of components (#15)
    * support booleans in Reconciler

---
+++ lib/Reconciler.spec.lua
@@ -0,0 +1,8 @@
+return function()
+	local Reconciler = require(script.Parent.Reconciler)
+
+	it("should reify booleans as nil", function()
+		local booleanReified = Reconciler.reify(false)
+		expect(booleanReified).to.never.be.ok()
+	end)
+end

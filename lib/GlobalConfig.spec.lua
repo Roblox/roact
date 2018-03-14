39956365e0dafc413e7de04750b81dee69a3b492
Lucien Greathouse <me@lpghatguy.com>
2018-03-14 11:24:04 -0700

    Move config implementation to Config, expose as GlobalConfig

--- lib/GlobalConfig.spec.lua
+++ lib/GlobalConfig.spec.lua
@@ -1,7 +1,9 @@
 return function()
 	local GlobalConfig = require(script.Parent.GlobalConfig)
 
-	it("TODO", function()
+	it("should have the correct methods", function()
 		expect(GlobalConfig).to.be.ok()
+		expect(GlobalConfig.set).to.be.ok()
+		expect(GlobalConfig.getValue).to.be.ok()
 	end)
 end

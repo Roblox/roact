4e773eff11aa14550ec78850e880fd728c8aec80
AmaranthineCodices <AmaranthineCodices@gmail.com>
2018-05-14 21:40:17 -0400

    add createRef function

---
+++ lib/createRef.lua
@@ -0,0 +1,14 @@
+--[[
+	Provides an API for acquiring a reference to a reified object. This
+	API is designed to mimic React 16.3's createRef API.
+
+	See:
+	* https://reactjs.org/docs/refs-and-the-dom.html
+	* https://reactjs.org/blog/2018/03/29/react-v-16-3.html#createref-api
+]]
+
+return function()
+	return {
+		current = nil,
+	}
+end

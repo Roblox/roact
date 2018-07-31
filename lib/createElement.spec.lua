4ee5d178addf173c6332b2952048c2402820b054
Lucien Greathouse <me@lpghatguy.com>
2018-07-31 16:01:07 -0700

    Expand createElement tests

--- lib/createElement.spec.lua
+++ lib/createElement.spec.lua
@@ -1,4 +1,6 @@
 return function()
+	local Component = require(script.Parent.Component)
+
 	local createElement = require(script.Parent.createElement)
 
 	it("should create new primitive elements", function()
@@ -15,7 +17,29 @@ return function()
 	end)
 
 	it("should create new stateful components", function()
-		local element = createElement({})
+		local Foo = Component:extend("Foo")
+
+		local element = createElement(Foo)
+
+		expect(element).to.be.ok()
+	end)
+
+	it("should accept props", function()
+		local element = createElement("StringValue", {
+			Value = "Foo",
+		})
+
+		expect(element).to.be.ok()
+	end)
+
+	it("should accept props and children", function()
+		local element = createElement("StringValue", {
+			Value = "Foo",
+		}, {
+			Child = createElement("IntValue", {
+				Value = 6,
+			}),
+		})
 
 		expect(element).to.be.ok()
 	end)

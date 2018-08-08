ccf21f4efee157d2b89cf09ed64881dfb34e8955
Lucien Greathouse <me@lpghatguy.com>
2018-08-08 12:18:17 -0700

    Start working on RobloxRenderer

--- lib/RobloxRenderer.lua
+++ lib/RobloxRenderer.lua
@@ -2,6 +2,21 @@ local Type = require(script.Parent.Type)
 local ElementKind = require(script.Parent.ElementKind)
 local Core = require(script.Parent.Core)
 
+local function setHostProperty(node, key, newValue, oldValue)
+	if newValue == oldValue then
+		return
+	end
+
+	local keyType = typeof(key)
+
+	if keyType == "string" then
+		node.instance[key] = newValue
+	else
+		-- TODO
+		error("NYI")
+	end
+end
+
 local RobloxRenderer = {}
 
 function RobloxRenderer.mountHostNode(reconciler, node, element, hostParent, key)
@@ -42,7 +57,26 @@ function RobloxRenderer.unmountHostNode(reconciler, node)
 end
 
 function RobloxRenderer.reconcileHostNode(reconciler, node, newElement)
-	-- TODO: Reconcile props
+	local oldProps = node.currentElement.props
+	local newProps = newElement.props
+
+	-- Apply props that were added or updated
+	for key, newValue in pairs(newProps) do
+		local oldValue = oldProps[key]
+
+		if newValue ~= oldValue then
+			setHostProperty(node, key, newValue, oldValue)
+		end
+	end
+
+	-- Apply props that were removed
+	for key, oldValue in pairs(oldProps) do
+		local newValue = newProps[key]
+
+		if newValue == nil then
+			setHostProperty(node, key, nil, oldValue)
+		end
+	end
 
 	reconciler.reconcileNodeChildren(node, newElement.props[Core.Children])
 

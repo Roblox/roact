ef681f3ce2a45dd68f96d38023daf3051e6e2125
AmaranthineCodices <git@amaranthinecodices.me>
2018-08-20 19:19:08 -0400

    helpful (?) comments

--- lib/ChildUtils.lua
+++ lib/ChildUtils.lua
@@ -6,8 +6,35 @@ end
 
 local ChildUtils = {}
 
+--[[
+	A signal value indicating that a child should use its parent's key, because
+	it has no key of its own.
+
+	This occurs when you return only one element from a functional component or
+	stateful render function.
+]]
 ChildUtils.UseParentKey = {}
 
+--[[
+	Returns an iterator over the children of an element.
+	`childrenOrChild` may be one of:
+	* a boolean
+	* nil
+	* a single element
+	* a table of elements
+
+	If `childrenOrChild` is a boolean or nil, this will return an iterator with
+	zero elements.
+
+	If `childrenOrChild` is a single element, this will return an iterator with
+	one element: a tuple where the first value is ChildUtils.UseParentKey, and
+	the second is the value of `childrenOrChild`.
+
+	If `childrenOrChild` is a table, this will return an iterator over all the
+	elements of the array, equivalent to `pairs(childrenOrChild)`.
+
+	If `childrenOrChild` is none of the above, this function will throw.
+]]
 function ChildUtils.iterateChildren(childrenOrChild)
 	local richType = Type.of(childrenOrChild)
 
@@ -40,6 +67,15 @@ function ChildUtils.iterateChildren(childrenOrChild)
 	error("Invalid children")
 end
 
+--[[
+	Gets the child corresponding to a given key, respecting Roact's rules for
+	children. Specifically:
+	* If `elements` is nil or a boolean, this will return `nil`, regardless of
+	  the key given.
+	* If `elements` is a single element, this will return `nil`, unless the key
+	  is ChildUtils.UseParentKey.
+	* If `elements` is a table of elements, this will return `elements[key]`.
+]]
 function ChildUtils.getChildByKey(elements, key)
 	if elements == nil or typeof(elements) == "boolean" then
 		return nil

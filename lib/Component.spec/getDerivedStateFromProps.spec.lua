6249b37b514c6d0927ca7143496f378b6454a8e8
Lucien Greathouse <me@lpghatguy.com>
2018-10-01 14:29:32 -0700

    defaultProps tests

---
+++ lib/Component.spec/getDerivedStateFromProps.spec.lua
@@ -0,0 +1,6 @@
+return function()
+	-- TODO: it should be invoked on initial mount
+	-- TODO: it should be invoked when updated via props
+	-- TODO: it should be invoked when updated via state
+	-- TODO: it should receive defaultProps
+end

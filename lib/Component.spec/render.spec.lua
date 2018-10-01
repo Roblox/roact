67ea282c58c3b8b270749ff63ea59004f56ebb86
Lucien Greathouse <me@lpghatguy.com>
2018-10-01 13:47:21 -0700

    Add render test for state changes

--- lib/Component.spec/render.spec.lua
+++ lib/Component.spec/render.spec.lua
@@ -29,7 +29,6 @@ return function()
 		local Foo = Component:extend("Foo")
 
 		local renderSpy = createSpy()
-
 		Foo.render = renderSpy.value
 
 		local element = createElement(Foo)
@@ -45,11 +44,10 @@ return function()
 		expect(Type.of(renderArguments.self)).to.equal(Type.StatefulComponentInstance)
 	end)
 
-	it("should be invoked when a component is updated", function()
+	it("should be invoked when a component is updated via props", function()
 		local Foo = Component:extend("Foo")
 
 		local renderSpy = createSpy()
-
 		Foo.render = renderSpy.value
 
 		local element = createElement(Foo)
@@ -68,5 +66,45 @@ return function()
 
 		local renderArguments = renderSpy:captureValues("self")
 		expect(Type.of(renderArguments.self)).to.equal(Type.StatefulComponentInstance)
+	end)
+
+	it("should be invoked when a component is updated via state", function()
+		local Foo = Component:extend("Foo")
+
+		local setState
+		function Foo:init()
+			setState = function(...)
+				return self:setState(...)
+			end
+		end
+
+		local renderSpy = createSpy()
+		Foo.render = renderSpy.value
+
+		local element = createElement(Foo)
+		local hostParent = nil
+		local key = "Foo Test"
+
+		noopReconciler.mountNode(element, hostParent, key)
+
+		expect(renderSpy.callCount).to.equal(1)
+
+		local initialRenderArguments = renderSpy:captureValues("self")
+		local initialProps = initialRenderArguments.self.props
+		local initialState = initialRenderArguments.self.state
+
+		expect(Type.of(initialRenderArguments.self)).to.equal(Type.StatefulComponentInstance)
+
+		setState({})
+
+		expect(renderSpy.callCount).to.equal(2)
+
+		local renderArguments = renderSpy:captureValues("self")
+		local props = renderArguments.self.props
+		local state = renderArguments.self.state
+
+		expect(Type.of(renderArguments.self)).to.equal(Type.StatefulComponentInstance)
+		expect(props).to.equal(initialProps)
+		expect(state).never.to.equal(initialState)
 	end)
 end

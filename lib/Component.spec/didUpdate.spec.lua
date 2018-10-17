f22fa762ff3416eb92e2d914bed372324f4e3428
Lucien Greathouse <me@lpghatguy.com>
2018-10-17 14:07:38 -0700

    Add didUpdate test for setState

--- lib/Component.spec/didUpdate.spec.lua
+++ lib/Component.spec/didUpdate.spec.lua
@@ -14,7 +14,6 @@ return function()
 		local MyComponent = Component:extend("MyComponent")
 
 		local didUpdateSpy = createSpy()
-
 		MyComponent.didUpdate = didUpdateSpy.value
 
 		function MyComponent:render()
@@ -28,14 +27,16 @@ return function()
 		local hostParent = nil
 		local key = "Test"
 
-		local node = noopReconciler.mountVirtualNode(initialElement, hostParent, key)
+		local virtualNode = noopReconciler.mountVirtualNode(initialElement, hostParent, key)
+
+		expect(didUpdateSpy.callCount).to.equal(0)
 
 		local newProps = {
 			a = 6,
 			b = 2,
 		}
 		local newElement = createElement(MyComponent, newProps)
-		noopReconciler.updateVirtualNode(node, newElement)
+		noopReconciler.updateVirtualNode(virtualNode, newElement)
 
 		expect(didUpdateSpy.callCount).to.equal(1)
 
@@ -46,5 +47,46 @@ return function()
 		assertDeepEqual(values.oldState, {})
 	end)
 
-	-- TODO: it should be invoked when updated via setState
+	it("should be invoked when updated via setState", function()
+		local MyComponent = Component:extend("MyComponent")
+
+		local didUpdateSpy = createSpy()
+		MyComponent.didUpdate = didUpdateSpy.value
+
+		local initialState = {
+			a = 4,
+		}
+
+		local setState
+		function MyComponent:init()
+			setState = function(...)
+				return self:setState(...)
+			end
+
+			self:setState(initialState)
+		end
+
+		function MyComponent:render()
+		end
+
+		local element = createElement(MyComponent)
+		local hostParent = nil
+		local key = "Test"
+
+		noopReconciler.mountVirtualNode(element, hostParent, key)
+
+		expect(didUpdateSpy.callCount).to.equal(0)
+
+		setState({
+			a = 5,
+		})
+
+		expect(didUpdateSpy.callCount).to.equal(1)
+
+		local values = didUpdateSpy:captureValues("self", "oldProps", "oldState")
+
+		expect(Type.of(values.self)).to.equal(Type.StatefulComponentInstance)
+		assertDeepEqual(values.oldProps, {})
+		assertDeepEqual(values.oldState, initialState)
+	end)
 end

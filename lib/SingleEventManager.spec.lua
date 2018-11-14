0520cca2f4373c88be60bd0c1ec2257d828babda
Lucien Greathouse <me@lpghatguy.com>
2018-11-14 15:54:09 -0800

    Add more SingleEventManager test TODOs

--- lib/SingleEventManager.spec.lua
+++ lib/SingleEventManager.spec.lua
@@ -69,11 +69,15 @@ return function()
 			expect(callCount).to.equal(1)
 		end)
 
+		-- TODO: Port tests to createSpy
+		-- TODO: Reduce property changed signal tests, since they overlap
 		-- TODO: Test that events fired during manager resumption get fired
 		-- TODO: Test that events fired during suspension and disconnected
 		-- before resume aren't fired
 		-- TODO: Test that events that yield don't yield through the manager
 		-- TODO: Test that events that throw don't throw through the manager
+		-- TODO: Test that manager:resume() fired from a suspended event
+		-- listener won't double-fire events.
 	end)
 
 	describe("connectPropertyChange", function()

3bd36377b48ff2db4ecdfe149784c5d1d9cacdaf
Lucien Greathouse <me@lpghatguy.com>
2018-11-15 17:56:44 -0800

    Upgrade TODO comments to itSKIP tests

--- lib/Component.spec/getDerivedStateFromProps.spec.lua
+++ lib/Component.spec/getDerivedStateFromProps.spec.lua
@@ -1,6 +1,6 @@
 return function()
-	-- TODO: it should be invoked on initial mount
-	-- TODO: it should be invoked when updated via props
-	-- TODO: it should be invoked when updated via state
-	-- TODO: it should receive defaultProps
+	itSKIP("should be invoked on initial mount", function() end)
+	itSKIP("should be invoked when updated via props", function() end)
+	itSKIP("should be invoked when updated via state", function() end)
+	itSKIP("should receive defaultProps", function() end)
 end

e4bc2baca439c599bbf318c7b6ee9fe810cd9d3e
Paul Doyle <pdoyle@roblox.com>
2019-03-26 16:47:23 -0700

    Introduce xpcall for properties set via bindings

--- lib/RobloxRenderer.lua
+++ lib/RobloxRenderer.lua
@@ -51,7 +51,6 @@ local function setRobloxInstanceProperty(hostObject, key, newValue)
 	end
 
 	-- Assign the new value to the object
-	-- TODO: Handle errors if `key` is not a valid Instance property
 	hostObject[key] = newValue
 
 	return
@@ -65,7 +64,20 @@ end
 
 local function attachBinding(virtualNode, key, newBinding)
 	local function updateBoundProperty(newValue)
-		setRobloxInstanceProperty(virtualNode.hostObject, key, newValue)
+		local success, errorMessage = xpcall(function()
+			setRobloxInstanceProperty(virtualNode.hostObject, key, newValue)
+		end, debug.traceback)
+
+		if not success then
+			local source = virtualNode.currentElement.source
+
+			if source == nil then
+				source = "<enable element tracebacks>"
+			end
+
+			local fullMessage = updatePropsError:format(errorMessage, source)
+			error(fullMessage, 0)
+		end
 	end
 
 	if virtualNode.bindings == nil then
@@ -74,7 +86,7 @@ local function attachBinding(virtualNode, key, newBinding)
 
 	virtualNode.bindings[key] = Binding.subscribe(newBinding, updateBoundProperty)
 
-	setRobloxInstanceProperty(virtualNode.hostObject, key, newBinding:getValue())
+	updateBoundProperty(newBinding:getValue())
 end
 
 local function detachAllBindings(virtualNode)

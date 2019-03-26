1ee3149619b9b4470c1dfb9a3fa9cf5462746df8
Lily Brown <git@amaranthinecodices.me>
2019-03-26 13:37:02 -0700

    reindent validateProps tests

--- lib/Component.spec/validateProps.spec.lua
+++ lib/Component.spec/validateProps.spec.lua
@@ -1,172 +1,172 @@
 return function()
-    local createElement = require(script.Parent.Parent.createElement)
-    local createReconciler = require(script.Parent.Parent.createReconciler)
-    local createSpy = require(script.Parent.Parent.createSpy)
-    local NoopRenderer = require(script.Parent.Parent.NoopRenderer)
+	local createElement = require(script.Parent.Parent.createElement)
+	local createReconciler = require(script.Parent.Parent.createReconciler)
+	local createSpy = require(script.Parent.Parent.createSpy)
+	local NoopRenderer = require(script.Parent.Parent.NoopRenderer)
 
-    local Component = require(script.Parent.Parent.Component)
+	local Component = require(script.Parent.Parent.Component)
 
-    local noopReconciler = createReconciler(NoopRenderer)
+	local noopReconciler = createReconciler(NoopRenderer)
 
-    it("should be invoked when mounted", function()
-        local MyComponent = Component:extend("MyComponent")
+	it("should be invoked when mounted", function()
+		local MyComponent = Component:extend("MyComponent")
 
-        local validatePropsSpy = createSpy(function()
-            return true
-        end)
+		local validatePropsSpy = createSpy(function()
+			return true
+		end)
 
-        MyComponent.validateProps = validatePropsSpy.value
+		MyComponent.validateProps = validatePropsSpy.value
 
-        function MyComponent:render()
-            return nil
-        end
+		function MyComponent:render()
+			return nil
+		end
 
-        local element = createElement(MyComponent)
+		local element = createElement(MyComponent)
 		local hostParent = nil
-        local key = "Test"
+		local key = "Test"
 
-        noopReconciler.mountVirtualNode(element, hostParent, key)
-        expect(validatePropsSpy.callCount).to.equal(1)
-    end)
+		noopReconciler.mountVirtualNode(element, hostParent, key)
+		expect(validatePropsSpy.callCount).to.equal(1)
+	end)
 
-    it("should be invoked when props change", function()
-        local MyComponent = Component:extend("MyComponent")
+	it("should be invoked when props change", function()
+		local MyComponent = Component:extend("MyComponent")
 
-        local validatePropsSpy = createSpy(function()
-            return true
-        end)
+		local validatePropsSpy = createSpy(function()
+			return true
+		end)
 
-        MyComponent.validateProps = validatePropsSpy.value
+		MyComponent.validateProps = validatePropsSpy.value
 
-        function MyComponent:render()
-            return nil
-        end
+		function MyComponent:render()
+			return nil
+		end
 
-        local element = createElement(MyComponent, { a = 1 })
+		local element = createElement(MyComponent, { a = 1 })
 		local hostParent = nil
-        local key = "Test"
+		local key = "Test"
 
-        local node = noopReconciler.mountVirtualNode(element, hostParent, key)
-        expect(validatePropsSpy.callCount).to.equal(1)
-        validatePropsSpy:assertCalledWithDeepEqual({
-            a = 1,
-        })
+		local node = noopReconciler.mountVirtualNode(element, hostParent, key)
+		expect(validatePropsSpy.callCount).to.equal(1)
+		validatePropsSpy:assertCalledWithDeepEqual({
+			a = 1,
+		})
 
-        local newElement = createElement(MyComponent, { a = 2 })
-        noopReconciler.updateVirtualNode(node, newElement)
-        expect(validatePropsSpy.callCount).to.equal(2)
-        validatePropsSpy:assertCalledWithDeepEqual({
-            a = 2,
-        })
-    end)
+		local newElement = createElement(MyComponent, { a = 2 })
+		noopReconciler.updateVirtualNode(node, newElement)
+		expect(validatePropsSpy.callCount).to.equal(2)
+		validatePropsSpy:assertCalledWithDeepEqual({
+			a = 2,
+		})
+	end)
 
-    it("should not be invoked when state changes", function()
-        local MyComponent = Component:extend("MyComponent")
+	it("should not be invoked when state changes", function()
+		local MyComponent = Component:extend("MyComponent")
 
-        local setStateCallback = nil
-        local validatePropsSpy = createSpy(function()
-            return true
-        end)
+		local setStateCallback = nil
+		local validatePropsSpy = createSpy(function()
+			return true
+		end)
 
-        MyComponent.validateProps = validatePropsSpy.value
+		MyComponent.validateProps = validatePropsSpy.value
 
-        function MyComponent:init()
-            setStateCallback = function(newState)
-                self:setState(newState)
-            end
-        end
+		function MyComponent:init()
+			setStateCallback = function(newState)
+				self:setState(newState)
+			end
+		end
 
-        function MyComponent:render()
-            return nil
-        end
+		function MyComponent:render()
+			return nil
+		end
 
-        local element = createElement(MyComponent, { a = 1 })
+		local element = createElement(MyComponent, { a = 1 })
 		local hostParent = nil
-        local key = "Test"
+		local key = "Test"
 
-        noopReconciler.mountVirtualNode(element, hostParent, key)
-        expect(validatePropsSpy.callCount).to.equal(1)
-        validatePropsSpy:assertCalledWithDeepEqual({
-            a = 1
-        })
+		noopReconciler.mountVirtualNode(element, hostParent, key)
+		expect(validatePropsSpy.callCount).to.equal(1)
+		validatePropsSpy:assertCalledWithDeepEqual({
+			a = 1
+		})
 
-        setStateCallback({
-            b = 1
-        })
+		setStateCallback({
+			b = 1
+		})
 
-        expect(validatePropsSpy.callCount).to.equal(1)
-    end)
+		expect(validatePropsSpy.callCount).to.equal(1)
+	end)
 
-    it("should throw if validateProps is not a function", function()
-        local MyComponent = Component:extend("MyComponent")
-        MyComponent.validateProps = 1
+	it("should throw if validateProps is not a function", function()
+		local MyComponent = Component:extend("MyComponent")
+		MyComponent.validateProps = 1
 
-        function MyComponent:render()
-            return nil
-        end
+		function MyComponent:render()
+			return nil
+		end
 
-        local element = createElement(MyComponent)
+		local element = createElement(MyComponent)
 		local hostParent = nil
-        local key = "Test"
+		local key = "Test"
 
-        expect(function()
-            noopReconciler.mountVirtualNode(element, hostParent, key)
-        end).to.throw()
-    end)
+		expect(function()
+			noopReconciler.mountVirtualNode(element, hostParent, key)
+		end).to.throw()
+	end)
 
-    it("should throw if validateProps returns false", function()
-        local MyComponent = Component:extend("MyComponent")
-        MyComponent.validateProps = function()
-            return false
-        end
+	it("should throw if validateProps returns false", function()
+		local MyComponent = Component:extend("MyComponent")
+		MyComponent.validateProps = function()
+			return false
+		end
 
-        function MyComponent:render()
-            return nil
-        end
+		function MyComponent:render()
+			return nil
+		end
 
-        local element = createElement(MyComponent)
+		local element = createElement(MyComponent)
 		local hostParent = nil
-        local key = "Test"
+		local key = "Test"
 
-        expect(function()
-            noopReconciler.mountVirtualNode(element, hostParent, key)
-        end).to.throw()
+		expect(function()
+			noopReconciler.mountVirtualNode(element, hostParent, key)
+		end).to.throw()
 	end)
 
 	it("should be invoked after defaultProps are applied", function()
-        local MyComponent = Component:extend("MyComponent")
+		local MyComponent = Component:extend("MyComponent")
 
-        local validatePropsSpy = createSpy(function()
-            return true
-        end)
+		local validatePropsSpy = createSpy(function()
+			return true
+		end)
 
-        MyComponent.validateProps = validatePropsSpy.value
+		MyComponent.validateProps = validatePropsSpy.value
 
-        function MyComponent:render()
-            return nil
+		function MyComponent:render()
+			return nil
 		end
 
 		MyComponent.defaultProps = {
 			b = 2,
 		}
 
-        local element = createElement(MyComponent, { a = 1 })
+		local element = createElement(MyComponent, { a = 1 })
 		local hostParent = nil
-        local key = "Test"
+		local key = "Test"
 
-        local node = noopReconciler.mountVirtualNode(element, hostParent, key)
-        expect(validatePropsSpy.callCount).to.equal(1)
-        validatePropsSpy:assertCalledWithDeepEqual({
+		local node = noopReconciler.mountVirtualNode(element, hostParent, key)
+		expect(validatePropsSpy.callCount).to.equal(1)
+		validatePropsSpy:assertCalledWithDeepEqual({
 			a = 1,
 			b = 2,
-        })
+		})
 
-        local newElement = createElement(MyComponent, { a = 2 })
-        noopReconciler.updateVirtualNode(node, newElement)
-        expect(validatePropsSpy.callCount).to.equal(2)
-        validatePropsSpy:assertCalledWithDeepEqual({
+		local newElement = createElement(MyComponent, { a = 2 })
+		noopReconciler.updateVirtualNode(node, newElement)
+		expect(validatePropsSpy.callCount).to.equal(2)
+		validatePropsSpy:assertCalledWithDeepEqual({
 			a = 2,
 			b = 2,
-        })
-    end)
+		})
+	end)
 end

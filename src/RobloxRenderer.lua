8e89c4202103e58dac7d877820c4070c002d1ee4
Lily <31936135+AmaranthineCodices@users.noreply.github.com>
2019-05-30 12:03:38 -0700

    Remove children when updating a host node to an element with nil children (#210)
    * Revert performance improvement
    
    This caused an issue when updating an element that had children - when
    an element went from having children to having nil children, the
    rendered objects would not be removed.
    
    * add test to verify that #209 is fixed
    
    * update changelog
    
    * Dial back bugfix. Reintroduce performance consideration, add additional check to resolve the bug.
    
    * Preempt review comments
    
    * Update src/RobloxRenderer.lua
    
    Co-Authored-By: Lucien Greathouse <me@lpghatguy.com>

--- src/RobloxRenderer.lua
+++ src/RobloxRenderer.lua
@@ -269,7 +269,7 @@ function RobloxRenderer.updateHostNode(reconciler, virtualNode, newElement)
 	end
 
 	local children = newElement.props[Children]
-	if children ~= nil then
+	if children ~= nil or oldProps[Children] ~= nil then
 		reconciler.updateVirtualNodeWithChildren(virtualNode, virtualNode.hostObject, children)
 	end
 

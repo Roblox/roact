cfd055a1135d67d435280acb91a7b199a981b18d
Olivier Trepanier <jeparlefrancais21@gmail.com>
2019-08-13 17:38:00 -0700

    Improve coverage of validateShallowOptions

--- src/shallow/init.lua
+++ src/shallow/init.lua
@@ -1,38 +1,10 @@
 local Type = require(script.Parent.Type)
 local ShallowWrapper = require(script.ShallowWrapper)
-
-local optionsTypes = {
-	depth = "number",
-}
-
-local function validateOptions(options)
-	if options == nil then
-		return true
-	end
-
-	for key, value in pairs(options) do
-		local expectType = optionsTypes[key]
-
-		if expectType == nil then
-			return false, ("unexpected option field %q (with value of %s)"):format(
-				tostring(key),
-				tostring(value)
-			)
-		elseif typeof(value) ~= expectType then
-			return false, ("unexpected option type for %q (expected %s but got %s)"):format(
-				tostring(key),
-				expectType,
-				typeof(value)
-			)
-		end
-	end
-
-	return true
-end
+local validateShallowOptions = require(script.validateShallowOptions)
 
 local function shallow(rootNode, options)
 	assert(Type.of(rootNode) == Type.VirtualNode, "Expected arg #1 to be a VirtualNode")
-	assert(validateOptions(options))
+	assert(validateShallowOptions(options))
 
 	options = options or {}
 	local maxDepth = options.depth or 1

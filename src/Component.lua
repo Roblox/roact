986c1d583bf378cf9cd9bd20f3fedc74f20ec3a4
Paul Doyle <37384169+ZoteTheMighty@users.noreply.github.com>
2019-08-14 09:39:08 -0700

    Fix inconsistent behavior between setState in init and directly assigning to state (#232)

--- src/Component.lua
+++ src/Component.lua
@@ -280,6 +280,7 @@ function Component:__mount(reconciler, virtualNode)
 
 	if instance.init ~= nil then
 		instance:init(instance.props)
+		assign(instance.state, instance:__getDerivedState(instance.props, instance.state))
 	end
 
 	-- It's possible for init() to redefine _context!

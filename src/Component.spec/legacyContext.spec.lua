68e09328c2c0547726cce0679a533cb8059be0a8
Paul Doyle <37384169+ZoteTheMighty@users.noreply.github.com>
2020-01-16 11:15:31 -0800

    New internal Context API (#247)
    Introduce a new internal Context api, exposed via Component and intended for use with a new public Context api

--- src/Component.spec/legacyContext.spec.lua
+++ src/Component.spec/legacyContext.spec.lua
@@ -3,17 +3,16 @@ return function()
 	local createElement = require(script.Parent.Parent.createElement)
 	local createReconciler = require(script.Parent.Parent.createReconciler)
 	local NoopRenderer = require(script.Parent.Parent.NoopRenderer)
-	local oneChild = require(script.Parent.Parent.oneChild)
 
 	local Component = require(script.Parent.Parent.Component)
 
 	local noopReconciler = createReconciler(NoopRenderer)
 
-	it("should be provided as an internal api on Component", function()
+	it("should be provided as a mutable self._context in Component:init", function()
 		local Provider = Component:extend("Provider")
 
 		function Provider:init()
-			self:__addContext("foo", "bar")
+			self._context.foo = "bar"
 		end
 
 		function Provider:render()
@@ -28,7 +27,7 @@ return function()
 			foo = "bar",
 		}
 
-		assertDeepEqual(node.context, expectedContext)
+		assertDeepEqual(node.legacyContext, expectedContext)
 	end)
 
 	it("should be inherited from parent stateful nodes", function()
@@ -36,10 +35,7 @@ return function()
 
 		local capturedContext
 		function Consumer:init()
-			capturedContext = {
-				hello = self:__getContext("hello"),
-				value = self:__getContext("value"),
-			}
+			capturedContext = self._context
 		end
 
 		function Consumer:render()
@@ -58,11 +54,11 @@ return function()
 			hello = "world",
 			value = 6,
 		}
-		local node = noopReconciler.mountVirtualNode(element, hostParent, hostKey, context)
+		local node = noopReconciler.mountVirtualNode(element, hostParent, hostKey, nil, context)
 
 		expect(capturedContext).never.to.equal(context)
-		expect(capturedContext).never.to.equal(node.context)
-		assertDeepEqual(node.context, context)
+		expect(capturedContext).never.to.equal(node.legacyContext)
+		assertDeepEqual(node.legacyContext, context)
 		assertDeepEqual(capturedContext, context)
 	end)
 
@@ -71,10 +67,7 @@ return function()
 
 		local capturedContext
 		function Consumer:init()
-			capturedContext = {
-				hello = self:__getContext("hello"),
-				value = self:__getContext("value"),
-			}
+			capturedContext = self._context
 		end
 
 		function Consumer:render()
@@ -91,82 +84,12 @@ return function()
 			hello = "world",
 			value = 6,
 		}
-		local node = noopReconciler.mountVirtualNode(element, hostParent, hostKey, context)
+		local node = noopReconciler.mountVirtualNode(element, hostParent, hostKey, nil, context)
 
 		expect(capturedContext).never.to.equal(context)
-		expect(capturedContext).never.to.equal(node.context)
-		assertDeepEqual(node.context, context)
+		expect(capturedContext).never.to.equal(node.legacyContext)
+		assertDeepEqual(node.legacyContext, context)
 		assertDeepEqual(capturedContext, context)
-	end)
-
-	it("should not copy the context table if it doesn't need to", function()
-		local Parent = Component:extend("Parent")
-
-		function Parent:init()
-			self:__addContext("parent", "I'm here!")
-		end
-
-		function Parent:render()
-			-- Create some child element
-			return createElement(function() end)
-		end
-
-		local element = createElement(Parent)
-		local hostParent = nil
-		local hostKey = "Parent"
-		local parentNode = noopReconciler.mountVirtualNode(element, hostParent, hostKey)
-
-		local expectedContext = {
-			parent = "I'm here!",
-		}
-
-		assertDeepEqual(parentNode.context, expectedContext)
-
-		local childNode = oneChild(parentNode.children)
-
-		-- Parent and child should have the same context table
-		expect(parentNode.context).to.equal(childNode.context)
-	end)
-
-	it("should not allow context to move up the tree", function()
-		local ChildProvider = Component:extend("ChildProvider")
-
-		function ChildProvider:init()
-			self:__addContext("child", "I'm here too!")
-		end
-
-		function ChildProvider:render()
-		end
-
-		local ParentProvider = Component:extend("ParentProvider")
-
-		function ParentProvider:init()
-			self:__addContext("parent", "I'm here!")
-		end
-
-		function ParentProvider:render()
-			return createElement(ChildProvider)
-		end
-
-		local element = createElement(ParentProvider)
-		local hostParent = nil
-		local hostKey = "Parent"
-
-		local parentNode = noopReconciler.mountVirtualNode(element, hostParent, hostKey)
-		local childNode = oneChild(parentNode.children)
-
-		local expectedParentContext = {
-			parent = "I'm here!",
-			-- Context does not travel back up
-		}
-
-		local expectedChildContext = {
-			parent = "I'm here!",
-			child = "I'm here too!"
-		}
-
-		assertDeepEqual(parentNode.context, expectedParentContext)
-		assertDeepEqual(childNode.context, expectedChildContext)
 	end)
 
 	it("should contain values put into the tree by parent nodes", function()
@@ -174,10 +97,7 @@ return function()
 
 		local capturedContext
 		function Consumer:init()
-			capturedContext = {
-				dont = self:__getContext("dont"),
-				frob = self:__getContext("frob"),
-			}
+			capturedContext = self._context
 		end
 
 		function Consumer:render()
@@ -186,7 +106,7 @@ return function()
 		local Provider = Component:extend("Provider")
 
 		function Provider:init()
-			self:__addContext("frob", "ulator")
+			self._context.frob = "ulator"
 		end
 
 		function Provider:render()
@@ -199,7 +119,7 @@ return function()
 		local context = {
 			dont = "try it",
 		}
-		local node = noopReconciler.mountVirtualNode(element, hostParent, hostKey, context)
+		local node = noopReconciler.mountVirtualNode(element, hostParent, hostKey, nil, context)
 
 		local initialContext = {
 			dont = "try it",
@@ -211,31 +131,23 @@ return function()
 		}
 
 		-- Because components mutate context, we're careful with equality
-		expect(node.context).never.to.equal(context)
+		expect(node.legacyContext).never.to.equal(context)
 		expect(capturedContext).never.to.equal(context)
-		expect(capturedContext).never.to.equal(node.context)
+		expect(capturedContext).never.to.equal(node.legacyContext)
 
 		assertDeepEqual(context, initialContext)
-		assertDeepEqual(node.context, expectedContext)
+		assertDeepEqual(node.legacyContext, expectedContext)
 		assertDeepEqual(capturedContext, expectedContext)
 	end)
 
 	it("should transfer context to children that are replaced", function()
 		local ConsumerA = Component:extend("ConsumerA")
 
-		local function captureAllContext(component)
-			return {
-				A = component:__getContext("A"),
-				B = component:__getContext("B"),
-				frob = component:__getContext("frob"),
-			}
-		end
-
 		local capturedContextA
 		function ConsumerA:init()
-			self:__addContext("A", "hello")
+			self._context.A = "hello"
 
-			capturedContextA = captureAllContext(self)
+			capturedContextA = self._context
 		end
 
 		function ConsumerA:render()
@@ -245,9 +157,9 @@ return function()
 
 		local capturedContextB
 		function ConsumerB:init()
-			self:__addContext("B", "hello")
+			self._context.B = "hello"
 
-			capturedContextB = captureAllContext(self)
+			capturedContextB = self._context
 		end
 
 		function ConsumerB:render()
@@ -256,7 +168,7 @@ return function()
 		local Provider = Component:extend("Provider")
 
 		function Provider:init()
-			self:__addContext("frob", "ulator")
+			self._context.frob = "ulator"
 		end
 
 		function Provider:render()
--- src/createReconciler.lua
+++ src/createReconciler.lua
@@ -31,16 +31,21 @@ local function createReconciler(renderer)
 		Unmount the given virtualNode, replacing it with a new node described by
 		the given element.
 
-		Preserves host properties, depth, and context from parent.
+		Preserves host properties, depth, and legacyContext from parent.
 	]]
 	local function replaceVirtualNode(virtualNode, newElement)
 		local hostParent = virtualNode.hostParent
 		local hostKey = virtualNode.hostKey
 		local depth = virtualNode.depth
-		local parentContext = virtualNode.parentContext
+
+		-- If the node that is being replaced has modified context, we need to
+		-- use the original *unmodified* context for the new node
+		-- The `originalContext` field will be nil if the context was unchanged
+		local context = virtualNode.originalContext or virtualNode.context
+		local parentLegacyContext = virtualNode.parentLegacyContext
 
 		unmountVirtualNode(virtualNode)
-		local newNode = mountVirtualNode(newElement, hostParent, hostKey, parentContext)
+		local newNode = mountVirtualNode(newElement, hostParent, hostKey, context, parentLegacyContext)
 
 		-- mountVirtualNode can return nil if the element is a boolean
 		if newNode ~= nil then
@@ -85,7 +90,13 @@ local function createReconciler(renderer)
 			end
 
 			if virtualNode.children[childKey] == nil then
-				local childNode = mountVirtualNode(newElement, hostParent, concreteKey, virtualNode.context)
+				local childNode = mountVirtualNode(
+					newElement,
+					hostParent,
+					concreteKey,
+					virtualNode.context,
+					virtualNode.legacyContext
+				)
 
 				-- mountVirtualNode can return nil if the element is a boolean
 				if childNode ~= nil then
@@ -247,10 +258,14 @@ local function createReconciler(renderer)
 	--[[
 		Constructs a new virtual node but not does mount it.
 	]]
-	local function createVirtualNode(element, hostParent, hostKey, context)
+	local function createVirtualNode(element, hostParent, hostKey, context, legacyContext)
 		if config.internalTypeChecks then
 			internalAssert(renderer.isHostObject(hostParent) or hostParent == nil, "Expected arg #2 to be a host object")
 			internalAssert(typeof(context) == "table" or context == nil, "Expected arg #4 to be of type table or nil")
+			internalAssert(
+				typeof(legacyContext) == "table" or legacyContext == nil,
+				"Expected arg #5 to be of type table or nil"
+			)
 		end
 		if config.typeChecks then
 			assert(hostKey ~= nil, "Expected arg #3 to be non-nil")
@@ -267,10 +282,21 @@ local function createReconciler(renderer)
 			children = {},
 			hostParent = hostParent,
 			hostKey = hostKey,
-			context = context,
-			-- This copy of context is useful if the element gets replaced
-			-- with an element of a different component type
-			parentContext = context,
+
+			-- Legacy Context API
+			-- A table of context values inherited from the parent node
+			legacyContext = legacyContext,
+
+			-- A saved copy of the parent context, used when replacing a node
+			parentLegacyContext = legacyContext,
+
+			-- Context API
+			-- A table of context values inherited from the parent node
+			context = context or {},
+
+			-- A saved copy of the unmodified context; this will be updated when
+			-- a component adds new context and used when a node is replaced
+			originalContext = nil,
 		}
 	end
 
@@ -304,10 +330,13 @@ local function createReconciler(renderer)
 		Constructs a new virtual node and mounts it, but does not place it into
 		the tree.
 	]]
-	function mountVirtualNode(element, hostParent, hostKey, context)
+	function mountVirtualNode(element, hostParent, hostKey, context, legacyContext)
 		if config.internalTypeChecks then
 			internalAssert(renderer.isHostObject(hostParent) or hostParent == nil, "Expected arg #2 to be a host object")
-			internalAssert(typeof(context) == "table" or context == nil, "Expected arg #4 to be of type table or nil")
+			internalAssert(
+				typeof(legacyContext) == "table" or legacyContext == nil,
+				"Expected arg #5 to be of type table or nil"
+			)
 		end
 		if config.typeChecks then
 			assert(hostKey ~= nil, "Expected arg #3 to be non-nil")
@@ -324,7 +353,7 @@ local function createReconciler(renderer)
 
 		local kind = ElementKind.of(element)
 
-		local virtualNode = createVirtualNode(element, hostParent, hostKey, context)
+		local virtualNode = createVirtualNode(element, hostParent, hostKey, context, legacyContext)
 
 		if kind == ElementKind.Host then
 			renderer.mountHostNode(reconciler, virtualNode)

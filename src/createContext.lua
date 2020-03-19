d1b1db25e3c4176f2b103d458711a3d968710b5c
Lucien Greathouse <me@lpghatguy.com>
2020-03-19 10:57:59 -0700

    Fix prop-context update tearing (#260)
    This PR tries to fix the update tearing behavior I discovered and reported in
    #259 based on a solution that my team has been discussing. I introduced a fairly
    small and robust test that is currently failing to indicate the problem with our
    current state and the quick-fix solutions to this problem.
    
    Context values are no longer based on bindings and now instead use a small cell
    that contains a value and an update signal.
    
    Providers now update context values in two passes:
    1. In `willUpdate`, the provider updates the value of the context entry but do
       not fire any signals. This should cause downstream consumers to be able to
       immediately read the new value on their next render. Their next render will
       occur immediately unless an intermediate component implements `shouldUpdate`.
    2. In `didUpdate`, the provider fires the signal portion of the context entry.
       This will notify any consumers.
    
    Consumers are now a little dumber and simpler. They no longer track the current
    context value in state, but instead track the last value they were updated with.
    When an update signal is received from their corresponding provider's
    `didUpdate` hook, consumers now compare the new value with the value they were
    last updated with, stored in their `didUpdate` hook. If these values differ, we
    can assume that there was a component between the consumer and provider that
    returned `false` to `shouldUpdate`, and trigger an update.
    
    I dramatically increased the documentation around affected code to make these
    kinds of issues more clear to us in the future.

--- src/createContext.lua
+++ src/createContext.lua
@@ -1,22 +1,50 @@
 local Symbol = require(script.Parent.Symbol)
-local Binding = require(script.Parent.Binding)
 local createFragment = require(script.Parent.createFragment)
+local createSignal = require(script.Parent.createSignal)
 local Children = require(script.Parent.PropMarkers.Children)
 local Component = require(script.Parent.Component)
+
+--[[
+	Construct the value that is assigned to Roact's context storage.
+]]
+local function createContextEntry(currentValue)
+	return {
+		value = currentValue,
+		onUpdate = createSignal(),
+	}
+end
 
 local function createProvider(context)
 	local Provider = Component:extend("Provider")
 
 	function Provider:init(props)
-		self.binding, self.updateValue = Binding.create(props.value)
+		self.contextEntry = createContextEntry(props.value)
+		self:__addContext(context.key, self.contextEntry)
+	end
 
-		local key = context.key
-		self:__addContext(key, self.binding)
+	function Provider:willUpdate(nextProps)
+		-- If the provided value changed, immediately update the context entry.
+		--
+		-- During this update, any components that are reachable will receive
+		-- this updated value at the same time as any props and state updates
+		-- that are being applied.
+		if nextProps.value ~= self.props.value then
+			self.contextEntry.value = nextProps.value
+		end
 	end
 
 	function Provider:didUpdate(prevProps)
+		-- If the provided value changed, after we've updated every reachable
+		-- component, fire a signal to update the rest.
+		--
+		-- This signal will notify all context consumers. It's expected that
+		-- they will compare the last context value they updated with and only
+		-- trigger an update on themselves if this value is different.
+		--
+		-- This codepath will generally only update consumer components that has
+		-- a component implementing shouldUpdate between them and the provider.
 		if prevProps.value ~= self.props.value then
-			self.updateValue(self.props.value)
+			self.contextEntry.onUpdate:fire(self.props.value)
 		end
 	end
 
@@ -30,29 +58,6 @@ end
 local function createConsumer(context)
 	local Consumer = Component:extend("Consumer")
 
-	function Consumer:init(props)
-		local key = context.key
-		local binding = self:__getContext(key)
-
-		if binding ~= nil then
-			self.state = {
-				value = binding:getValue(),
-			}
-
-			-- Update if the Context updated
-			self.disconnect = Binding.subscribe(binding, function()
-				self:setState({
-					value = binding:getValue(),
-				})
-			end)
-		else
-			-- Fall back to the default value if no Provider exists
-			self.state = {
-				value = context.defaultValue,
-			}
-		end
-	end
-
 	function Consumer.validateProps(props)
 		if type(props.render) ~= "function" then
 			return false, "Consumer expects a `render` function"
@@ -61,8 +66,52 @@ local function createConsumer(context)
 		end
 	end
 
+	function Consumer:init(props)
+		-- This value may be nil, which indicates that our consumer is not a
+		-- descendant of a provider for this context item.
+		self.contextEntry = self:__getContext(context.key)
+	end
+
 	function Consumer:render()
-		return self.props.render(self.state.value)
+		-- Render using the latest available for this context item.
+		--
+		-- We don't store this value in state in order to have more fine-grained
+		-- control over our update behavior.
+		local value
+		if self.contextEntry ~= nil then
+			value = self.contextEntry.value
+		else
+			value = context.defaultValue
+		end
+
+		return self.props.render(value)
+	end
+
+	function Consumer:didUpdate()
+		-- Store the value that we most recently updated with.
+		--
+		-- This value is compared in the contextEntry onUpdate hook below.
+		self.lastValue = self.contextEntry.value
+	end
+
+	function Consumer:didMount()
+		if self.contextEntry ~= nil then
+			-- When onUpdate is fired, a new value has been made available in
+			-- this context entry, but we may have already updated in the same
+			-- update cycle.
+			--
+			-- To avoid sending a redundant update, we compare the new value
+			-- with the last value that we updated with (set in didUpdate) and
+			-- only update if they differ. This may happen when an update from a
+			-- provider was blocked by an intermediate component that returned
+			-- false from shouldUpdate.
+			self.disconnect = self.contextEntry.onUpdate:subscribe(function(newValue)
+				if newValue ~= self.lastValue then
+					-- Trigger a dummy state update.
+					self:setState({})
+				end
+			end)
+		end
 	end
 
 	function Consumer:willUnmount()
@@ -78,12 +127,10 @@ local Context = {}
 Context.__index = Context
 
 function Context.new(defaultValue)
-	local self = {
+	return setmetatable({
 		defaultValue = defaultValue,
 		key = Symbol.named("ContextKey"),
-	}
-	setmetatable(self, Context)
-	return self
+	}, Context)
 end
 
 function Context:__tostring()
@@ -92,6 +139,7 @@ end
 
 local function createContext(defaultValue)
 	local context = Context.new(defaultValue)
+
 	return {
 		Provider = createProvider(context),
 		Consumer = createConsumer(context),
--- src/createContext.spec.lua
+++ src/createContext.spec.lua
@@ -1,7 +1,10 @@
 return function()
+	local Component = require(script.Parent.Component)
+	local NoopRenderer = require(script.Parent.NoopRenderer)
+	local Children = require(script.Parent.PropMarkers.Children)
 	local createContext = require(script.Parent.createContext)
 	local createElement = require(script.Parent.createElement)
-	local NoopRenderer = require(script.Parent.NoopRenderer)
+	local createFragment = require(script.Parent.createFragment)
 	local createReconciler = require(script.Parent.createReconciler)
 	local createSpy = require(script.Parent.createSpy)
 
@@ -113,10 +116,168 @@ return function()
 				Listener = createElement(Listener),
 			}))
 
-			expect(valueSpy.callCount).to.equal(3)
+			expect(valueSpy.callCount).to.equal(2)
 			valueSpy:assertCalledWith("ThirdTest")
 
 			noopReconciler.unmountVirtualTree(tree)
+		end)
+
+		--[[
+			This test is the same as the one above, but with a component that
+			always blocks updates in the middle. We expect behavior to be the
+			same.
+		]]
+		it("should update when the value updates through an update blocking component", function()
+			local valueSpy = createSpy()
+			local context = createContext("Test")
+
+			local UpdateBlocker = Component:extend("UpdateBlocker")
+
+			function UpdateBlocker:render()
+				return createFragment(self.props[Children])
+			end
+
+			function UpdateBlocker:shouldUpdate()
+				return false
+			end
+
+			local function Listener()
+				return createElement(context.Consumer, {
+					render = valueSpy.value,
+				})
+			end
+
+			local element = createElement(context.Provider, {
+				value = "NewTest",
+			}, {
+				Blocker = createElement(UpdateBlocker, nil, {
+					Listener = createElement(Listener),
+				}),
+			})
+
+			local tree = noopReconciler.mountVirtualTree(element, nil, "Provide Tree")
+
+			expect(valueSpy.callCount).to.equal(1)
+			valueSpy:assertCalledWith("NewTest")
+
+			noopReconciler.updateVirtualTree(tree, createElement(context.Provider, {
+				value = "ThirdTest",
+			}, {
+				Blocker = createElement(UpdateBlocker, nil, {
+					Listener = createElement(Listener),
+				}),
+			}))
+
+			expect(valueSpy.callCount).to.equal(2)
+			valueSpy:assertCalledWith("ThirdTest")
+
+			noopReconciler.unmountVirtualTree(tree)
+		end)
+	end)
+
+	describe("Update order", function()
+		--[[
+			This test ensures that there is no scenario where we can observe
+			'update tearing' when props and context are updated at the same
+			time.
+
+			Update tearing is scenario where a single update is partially
+			applied in multiple steps instead of atomically. This is observable
+			by components and can lead to strange bugs or errors.
+
+			This instance of update tearing happens when updating a prop and a
+			context value in the same update. Image we represent our tree's
+			state as the current prop and context versions. Our initial state
+			is:
+
+			(prop_1, context_1)
+
+			The next state we would like to update to is:
+
+			(prop_2, context_2)
+
+			Under the bug reported in issue 259, Roact reaches three different
+			states in sequence:
+
+			1: (prop_1, context_1) - the initial state
+			2: (prop_2, context_1) - woops!
+			3: (prop_2, context_2) - correct end state
+
+			In state 2, a user component was added that tried to access the
+			current context value, which was not set at the time. This raised an
+			error, because this state is not valid!
+
+			The first proposed solution was to move the context update to happen
+			before the props update. It is easy to show that this will still
+			result in update tearing:
+
+			1: (prop_1, context_1)
+			2: (prop_1, context_2)
+			3: (prop_2, context_2)
+
+			Although the initial concern about newly added components observing
+			old context values is fixed, there is still a state
+			desynchronization between props and state.
+
+			We would instead like the following update sequence:
+
+			1: (prop_1, context_1)
+			2: (prop_2, context_2)
+
+			This test tries to ensure that is the case.
+
+			The initial bug report is here:
+			https://github.com/Roblox/roact/issues/259
+		]]
+		it("should update context at the same time as props", function()
+			-- These values are used to make sure we reach both the first and
+			-- second state combinations we want to visit.
+			local observedA = false
+			local observedB = false
+			local updateCount = 0
+
+			local context = createContext("default")
+
+			local function Listener(props)
+				return createElement(context.Consumer, {
+					render = function(value)
+						updateCount = updateCount + 1
+
+						if value == "context_1" then
+							expect(props.someProp).to.equal("prop_1")
+							observedA = true
+						elseif value == "context_2" then
+							expect(props.someProp).to.equal("prop_2")
+							observedB = true
+						else
+							error("Unexpected context value")
+						end
+					end,
+				})
+			end
+
+			local element1 = createElement(context.Provider, {
+				value = "context_1",
+			}, {
+				Child = createElement(Listener, {
+					someProp = "prop_1",
+				}),
+			})
+
+			local element2 = createElement(context.Provider, {
+				value = "context_2",
+			}, {
+				Child = createElement(Listener, {
+					someProp = "prop_2",
+				}),
+			})
+
+			local tree = noopReconciler.mountVirtualTree(element1, nil, "UpdateObservationIsFun")
+			noopReconciler.updateVirtualTree(tree, element2)
+
+			expect(updateCount).to.equal(2)
+			expect(observedA).to.equal(true)
+			expect(observedB).to.equal(true)
 		end)
 	end)
 end

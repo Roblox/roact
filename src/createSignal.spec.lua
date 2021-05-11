ad57c97361da24386ed53fd725c3b3a6426d629e
Jesse <yjia@roblox.com>
2021-05-10 17:51:50 -0700

    add test to cover multiple registering same listener

--- src/createSignal.spec.lua
+++ src/createSignal.spec.lua
@@ -118,4 +118,37 @@ return function()
 
 		expect(spyA.callCount).to.equal(1)
 	end)
+
+	it("should have one connection instance when add the same listener multiple times", function()
+		local signal = createSignal()
+
+		local spyA = createSpy()
+		local disconnect1 = signal:subscribe(spyA.value)
+
+		expect(spyA.callCount).to.equal(0)
+
+		local a = {}
+		local b = 67
+		signal:fire(a, b)
+
+		expect(spyA.callCount).to.equal(1)
+		spyA:assertCalledWith(a, b)
+
+		local disconnect2 = signal:subscribe(spyA.value)
+
+		signal:fire(b, a)
+		expect(spyA.callCount).to.equal(2)
+		spyA:assertCalledWith(b, a)
+
+		disconnect2()
+
+		signal:fire(a)
+
+		expect(spyA.callCount).to.equal(2)
+
+		-- should have no effect.
+		disconnect1()
+		signal:fire(a)
+		expect(spyA.callCount).to.equal(2)
+	end)
 end
